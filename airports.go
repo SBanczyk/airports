65f1ca6c9292190890bef89e29c414a555545b8d
Dominik Pawlak <dpawlak@sq3-partners.com>
2021-03-02 16:13:13 +0100

    Change file separator (Wikidata id has : ) (#4)

--- airports.go
+++ airports.go
@@ -40,7 +40,7 @@ func main() {
 		iata := record[3]
 		point := record[4]
 		lat, lon := calculatePosition(point)
-		_, err1 := fmt.Fprintf(file1, "%v:%v:%v:%v:%v:%v\n", item, itemLabel, icao, iata, lat, lon)
+		_, err1 := fmt.Fprintf(file1, "%v|%v|%v|%v|%v|%v\n", item, itemLabel, icao, iata, lat, lon)
 		if err1 != nil {
 			log.Fatal(err1)
 		}
--- airports_internal_test.go
+++ airports_internal_test.go
@@ -1,15 +1,16 @@
 package main
+
 import "testing"
 import "github.com/stretchr/testify/assert"
 
-func TestPositionCalculation(t *testing.T){
-	lat,lon := calculatePosition("Point(120.210435142 60.38344912)")
-	assert.EqualValues(t,"60.38344912",lat)
-	assert.EqualValues(t,"120.210435142",lon)
-	lat,lon = calculatePosition("")
-	assert.EqualValues(t,"",lat)
-	assert.EqualValues(t,"",lon)
-	lat,lon = calculatePosition("t2177715128")
-	assert.EqualValues(t,"",lat)
-	assert.EqualValues(t,"",lon)
+func TestPositionCalculation(t *testing.T) {
+	lat, lon := calculatePosition("Point(120.210435142 60.38344912)")
+	assert.EqualValues(t, "60.38344912", lat)
+	assert.EqualValues(t, "120.210435142", lon)
+	lat, lon = calculatePosition("")
+	assert.EqualValues(t, "", lat)
+	assert.EqualValues(t, "", lon)
+	lat, lon = calculatePosition("t2177715128")
+	assert.EqualValues(t, "", lat)
+	assert.EqualValues(t, "", lon)
 }
