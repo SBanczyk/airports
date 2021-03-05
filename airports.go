6119b88139e0121038902317965141c937815d65
SBanczyk <79901104+SBanczyk@users.noreply.github.com>
2021-03-05 09:01:34 +0100

    Duplicates (#7)
    * filter
    
    * filter
    
    * repaired
    
    * Duplicates
    
    * duplicates
    
    * code refactoring
    
    * final
    
    Co-authored-by: SBanczyk <sbanczyk@interia.pl>

--- airports.go
+++ airports.go
@@ -4,33 +4,48 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
 	"strings"
-	"net/http"
 )
 func main() {
-	resp, err := http.Get("http://localhost:8080/dir/query.csv")
+	fmt.Printf("Requesting Data from Wikidata\n")
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://query.wikidata.org/sparql?query=SELECT%20%3Fitem%20%3FitemLabel%20%3FICAO_code%20%3FIATA_code%20%3Fcoordinates%20WHERE%20%7B%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%2Cen%22.%20%7D%0A%20%20%3Fitem%20wdt%3AP239%20%3FICAO_code.%0A%20%20FILTER%28REGEX%28STR%28%3FICAO_code%29%2C%20%22%5E%5BA-Z0-9%5D%7B4%7D%24%22%29%29%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP238%20%3FIATA_code.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP625%20%3Fcoordinates.%20%7D%0A%20%20MINUS%20%7B%20%3Fitem%20wdt%3AP582%20%3Fend_time.%20%7D%0A%20%20minus%20%7B%20%3Fitem%20wdt%3AP576%20%3Fdissolved__abolished_or_demolished_date.%20%7D%0A%7D%0Aorder%20by%20%3FICAO_code", nil)
+	req.Header.Add("User-Agent", "github.com/SBanczyk/airports")
+	req.Header.Add("Accept", "text/csv")
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Panic(err)
 	}
 	defer resp.Body.Close()
 	r := csv.NewReader(resp.Body)
-	file1, f1 := os.Create("airports.db")
-	if f1 != nil {
-		log.Fatal(f1)
+	outputFile, err := os.Create("airports.db")
+	if err != nil {
+		log.Panic(err)
 	}
-	defer file1.Close()
-	flaga := false
+	duplicateFile, err := os.Create("airports_duplicate.db")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer duplicateFile.Close()
+	defer outputFile.Close()
+	firstLinePassed := false
+	recordCounter := 0
+	duplicate := duplicateCounter{duplicateFile: duplicateFile}
 	for {
 		record, err := r.Read()
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
-			log.Fatal(err)
+			log.Panic(err)
 		}
-		if !flaga {
-			flaga = true
+		if !firstLinePassed {
+			firstLinePassed = true
 			continue
 		}
 		item := record[0]
@@ -38,16 +53,20 @@ func main() {
 		icao := record[2]
 		iata := record[3]
 		point := record[4]
-		if len(iata) !=3 {
-			iata=""
+		if len(iata) != 3 {
+			iata = ""
 		}
 		lat, lon := calculatePosition(point)
-		_, err1 := fmt.Fprintf(file1, "%v|%v|%v|%v|%v|%v\n", item, itemLabel, icao, iata, lat, lon)
+		_, err1 := fmt.Fprintf(outputFile, "%v|%v|%v|%v|%v|%v\n", item, itemLabel, icao, iata, lat, lon)
+		recordCounter++
 		if err1 != nil {
-			log.Fatal(err1)
+			log.Panic(err1)
 		}
+		duplicate.checkIcao(icao)
 	}
+	fmt.Printf("Records: %v | Duplicates: %v\n", recordCounter, duplicate.duplicateCounter)
 }
+
 func calculatePosition(point string) (string, string) {
 	if !strings.HasPrefix(point, "Point") {
 		return "", ""
@@ -55,4 +74,27 @@ func calculatePosition(point string) (string, string) {
 	position := point[6:strings.Index(point, ")")]
 	latlon := strings.Split(position, " ")
 	return latlon[1], latlon[0]
+}
+
+type duplicateCounter struct {
+	lastIcao         string
+	duplicateCounter int
+	duplicateFile    *os.File
+	isDuplicate      bool
+}
+
+func (c *duplicateCounter) checkIcao(icao string) {
+	if c.lastIcao == icao && !c.isDuplicate {
+		c.duplicateCounter++
+		c.isDuplicate = true
+		_, err2 := fmt.Fprintf(c.duplicateFile, "%v\n", icao)
+		if err2 != nil {
+			log.Panic(err2)
+		}
+	} else if c.lastIcao == icao {
+		c.isDuplicate = true
+	} else {
+		c.isDuplicate = false
+	}
+	c.lastIcao = icao
 }
--- airports_internal_test.go
+++ airports_internal_test.go
@@ -1,7 +1,10 @@
 package main
 
-import "testing"
-import "github.com/stretchr/testify/assert"
+import (
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"testing"
+)
 
 func TestPositionCalculation(t *testing.T) {
 	lat, lon := calculatePosition("Point(120.210435142 60.38344912)")
@@ -13,4 +16,33 @@ func TestPositionCalculation(t *testing.T) {
 	lat, lon = calculatePosition("t2177715128")
 	assert.EqualValues(t, "", lat)
 	assert.EqualValues(t, "", lon)
+}
+func TestDuplicateCounter(t *testing.T) {
+	duplicate := duplicateCounter{}
+	file, err := ioutil.TempFile(t.TempDir(), "airports_duplicate_test.db")
+	if err != nil {
+		t.Fatalf("unable to create temporary file for testing")
+	}
+	duplicate.duplicateFile = file
+	duplicate.checkIcao("XGRT")
+	assert.EqualValues(t, 0, duplicate.duplicateCounter)
+	duplicate.checkIcao("XXYZ")
+	duplicate.checkIcao("XXYZ")
+	assert.EqualValues(t, 1, duplicate.duplicateCounter)
+	duplicate.checkIcao("XXZZ")
+	assert.EqualValues(t, 1, duplicate.duplicateCounter)
+	duplicate.checkIcao("XZZZ")
+	duplicate.checkIcao("XZZZ")
+	duplicate.checkIcao("XZZZ")
+	duplicate.checkIcao("XZZZ")
+	assert.EqualValues(t, 2, duplicate.duplicateCounter)
+	duplicate.checkIcao("ZZZZ")
+	duplicate.checkIcao("ZZZZ")
+	assert.EqualValues(t, 3, duplicate.duplicateCounter)
+	duplicate.checkIcao("YYYY")
+	duplicate.checkIcao("YYYY")
+	duplicate.checkIcao("YYYY")
+	duplicate.checkIcao("YYYY")
+	duplicate.checkIcao("YYYY")
+	assert.EqualValues(t, 4, duplicate.duplicateCounter)
 }
